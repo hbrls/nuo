afdbdc67c0c3a3206e7c70f1835574aff370d0b3
hbrls <shuaizhexu@gmail.com>
2022-09-06 16:07:57 +0800

    R: init

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="/rsrc/css/bootstrap-xs-e3fc08d519.css" />
+        </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
